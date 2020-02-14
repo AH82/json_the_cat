77abbe2fd6f0b240a525fbeabd8c8a775a6b118f
Hatem <hatem.deux@gmail.com>
2020-02-13 20:21:48 -0500

    index file created now calling breedfetcher. Necessary code implementation applied

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,20 +1,25 @@
-const userInput = process.argv.slice(2);
-const request = require('request');
-request('https://api.thecatapi.com/v1/breeds/search?q=' + userInput, (error, response, body) => {
-  // console.log(typeof body)
-  // console.log('response: ', response);
-  if (response === undefined) {
-    console.log('No response from the website! make sure you typed the correct address.');
+// const userInput = process.argv.slice(2); // userInput became breedName
+const fetchBreedDescription = function(breedName, callback) {
 
-  } else {
-    const data = JSON.parse(body);
-    // console.log('type of data [0]: ', typeof data[0]);
-    // console.log('error:', error);
-    if (data[0] === undefined) {
-      console.log(`The "${userInput}" does not exist (at least in our database), check for typos or try again with another breed.`);
+  const request = require('request');
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    // console.log(typeof body)
+    console.log('response: ', response && response.statusCode);
+    console.log('error:', error);
+    if (response === undefined) {
+      callback('No response from the website! make sure you typed the correct address.', null);
+  
     } else {
-      console.log(data[0]['description']);
+      const data = JSON.parse(body);
+      // console.log('type of data [0]: ', typeof data[0]);
+      if (data[0] === undefined) {
+      // if (data === null) {                // <-- this works in happy scenario
+        callback(`The "${breedName}" does not exist (at least in our database), check for typos or try again with another breed.`, null);
+      } else {
+        callback(null, data[0]['description']);
+      }
     }
-  }
- 
-});
+   
+  });
+};
+module.exports = { fetchBreedDescription };
---
+++ index.js
@@ -0,0 +1,13 @@
+// const userInput = process.argv.slice(2);
+// index.js
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
